39ac6e6d48ba37f99ac9a94607b8e16f9135f6a3
Vlad Savikin <vlad@savikin.ru>
2025-12-02 18:51:41 +0300

    Docker updates

--- backend/internal/db/init.sql
+++ backend/internal/db/init.sql
@@ -39,17 +39,6 @@ CREATE TABLE IF NOT EXISTS fs_test_summaries (
     fs_execution_time INTERVAL
 );
 
-CREATE INDEX idx_tags_name ON tags(name);
-CREATE INDEX idx_run_tags_run_id ON run_tags(run_id);
-CREATE INDEX idx_run_tags_tag_id ON run_tags(tag_id);
-CREATE INDEX idx_op_crashes_run_id ON op_crashes(run_id);
-CREATE INDEX idx_op_crashes_operation ON op_crashes(operation);
-CREATE INDEX idx_test_cases_crash_id ON test_cases(crash_id);
-CREATE INDEX idx_fs_summaries_test_case ON fs_test_summaries(test_case_id);
-CREATE INDEX idx_test_artifacts_test_case ON test_artifacts(test_case_id);
-CREATE INDEX idx_fs_artifacts_test_artifacts ON fs_artifacts(test_artifacts_id);
-
-
 
 INSERT INTO tags (name) VALUES 
 ('ext4'), ('xfs')
